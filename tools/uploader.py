4fbf05ea12a9e3b663c20f313c6a8dbf261f3270
David Manuel <dmanuel@fionta.com>
2022-02-19 09:52:44 -0800

    updates to package uploader tool for development

--- tools/uploader.py
+++ tools/uploader.py
@@ -3,11 +3,26 @@ import sys
 import os
 import requests
 load_dotenv()
-
 from enum import Enum
+import webbrowser
+
+
 class CodeType(Enum):
     APP = "app"
     DRIVER = "driver"
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 """WARNING!!! You must update these ids and set put hubitat ip address in a .env file to use this"""
 TARGETS = {
@@ -31,10 +46,12 @@ base_url = f"http://{ip}"
 
 
 def update_package(target_name, apps=True, drivers=True):
+    print(f"""Starting Upload to Hubitat""")
     return do_on_package(update_latest_version, target_name, apps, drivers)
 
 
 def retrieve_package(target_name, apps=True, drivers=True):
+    print(f"""Starting Retrieve from Hubitat""")
     return do_on_package(retrieve_latest_source, target_name, apps, drivers)
 
 
@@ -110,15 +127,35 @@ def get_info(id, type):
     return r.json(), r
 
 
+def open_all(pkg):
+    for app in TARGETS.get(pkg)['apps']:
+        open_in_hubitat_code_editor(CodeType.APP, app['id'])
+    for driver in TARGETS.get(pkg)['drivers']:
+        open_in_hubitat_code_editor(CodeType.DRIVER, driver['id'])
+
+
+def open_in_hubitat_code_editor(type, id):
+    url = f"""{base_url}/{type.value}/editor/{id}"""
+    webbrowser.open(url)
+
+
 if len(sys.argv) > 1:
     package_name = sys.argv[1]
+    direction = 'upload' if len(sys.argv) == 2 and sys.argv[2] == 'upload' else 'retrieve'
 else:
     package_name = os.getenv('TARGET')
+    direction = os.getenv('DIRECTION')
+
+
 
 if __name__ == "__main__":
     if not package_name:
         print("Warning! you must update the app id/driver ids to match your device.")
-        # update_package('MoenFloManager')
-        retrieve_package('MoenFloManager')
     else:
-        update_package(package_name)
+        if direction == 'upload':
+            update_package(package_name)
+        else:
+            retrieve_package(package_name)
+
+
+
