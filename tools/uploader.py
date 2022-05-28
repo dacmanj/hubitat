5cd899288c2cb5390b614164be8d6f41222f5729
David Manuel <dmanuel@fionta.com>
2022-05-28 07:51:46 -0700

    watchdog / auto upload

--- tools/uploader.py
+++ tools/uploader.py
@@ -2,9 +2,14 @@ from dotenv import load_dotenv
 import sys
 import os
 import requests
-load_dotenv()
-from enum import Enum
+from enum import Enum, auto
 import webbrowser
+load_dotenv()
+
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
 
 class CodeType(Enum):
@@ -22,6 +27,12 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+
+STATUS_COLOR = {
+    "success": bcolors.OKGREEN,
+    "error": bcolors.FAIL
+}
 
 
 """WARNING!!! You must update these ids and set put hubitat ip address in a .env file to use this"""
@@ -46,12 +57,12 @@ base_url = f"http://{ip}"
 
 
 def update_package(target_name, apps=True, drivers=True):
-    print(f"""Starting Upload to Hubitat""")
+    print(f"""{bcolors.HEADER}STARTING UPLOAD{bcolors.ENDC}""")
     return do_on_package(update_latest_version, target_name, apps, drivers)
 
 
 def retrieve_package(target_name, apps=True, drivers=True):
-    print(f"""Starting Retrieve from Hubitat""")
+    print(f"""{bcolors.HEADER}STARTING RETRIEVE{bcolors.ENDC}""")
     return do_on_package(retrieve_latest_source, target_name, apps, drivers)
 
 
@@ -73,15 +84,20 @@ def update_latest_version(id, source_path, type):
     display_id_name = ' '.join([str(x) for x in [id, name] if x])
     source = get_source(source_path)
     if source == info.get('source'):
-        print(f"{type.title()} {display_id_name} v{current_version} is the same as {source_path}: SKIPPED")
+        print(f"{bcolors.OKCYAN}SKIPPED{bcolors.ENDC}: {type.title()} {display_id_name} v{current_version} is the same as {source_path}")
     else:
-        print(f"Updating {type.title()} {display_id_name} v{current_version} with {source_path}")
+        print(f"{bcolors.BOLD}UPDATING:{bcolors.ENDC} {type.title()} {display_id_name} v{current_version} with {source_path}")
         result = update_code(id, source, current_version, type)
         status_code = result[1].status_code
         result_json = result[0]
         new_version = result_json.get('version')
-        status = f"{result_json['status']}: {result_json['errorMessage']}" if result_json.get('status') == 'error' else result_json['status']
-        print(f"Update of {type.title()} {display_id_name} v{new_version} {status_code}: {status}")
+        status = result_json['status']
+        status_color = STATUS_COLOR.get(status)
+        print(f"{status_color}{status.upper()}{bcolors.ENDC}: Update of {type.title()} {display_id_name} v{new_version} {status_code}:")
+        if status == 'error':
+            error_message = result_json.get('errorMessage').replace('\n', '')
+            print(f"\t{status_color}{source_path}{bcolors.ENDC}")
+            print(f"\t\t{status_color}{error_message}{bcolors.ENDC}") 
         return result
 
 
@@ -141,21 +157,49 @@ def open_in_hubitat_code_editor(type, id):
 
 if len(sys.argv) > 1:
     package_name = sys.argv[1]
-    direction = 'upload' if len(sys.argv) == 2 and sys.argv[2] == 'upload' else 'retrieve'
+    direction = 'upload' if len(sys.argv) == 3 and sys.argv[2] == 'upload' else 'retrieve'
 else:
     package_name = os.getenv('TARGET')
     direction = os.getenv('DIRECTION')
+    auto_upload = os.getenv('AUTOUPLOAD')
 
+
+from watchdog.events import FileSystemEventHandler
+
+class WatchdogModificationHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.event_type == 'modified':
+            package_data = TARGETS.get(package_name)
+            src_path = os.path.relpath(event.src_path)
+            app_modified = [update_latest_version(x['id'], src_path, 'app') for x in package_data['apps'] if x.get('source_path') == src_path]
+            driver_modified = [update_latest_version(x['id'], src_path, 'driver') for x in package_data['drivers'] if x.get('source_path') == src_path]
+
+def auto_upload():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = WatchdogModificationHandler()
+    observer = Observer()
+    path = '.'
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 
 if __name__ == "__main__":
     if not package_name:
         print("Warning! you must update the app id/driver ids to match your device.")
     else:
-        if direction == 'upload':
-            update_package(package_name)
+        if auto_upload:
+            auto_upload()
         else:
-            retrieve_package(package_name)
-
-
+            if direction == 'upload':
+                update_package(package_name)
+            else:
+                retrieve_package(package_name)
 
